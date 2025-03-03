cd075375b943fbfac62a2a509da1664744a1f904
Yura Shaplavsky <yurikorock777@gmail.com>
2025-03-03 22:10:40 +0200

    add start  return markup

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,8 +1,17 @@
 const gallery = document.querySelector('.gallery');
 
 
-import function renderGallery() {
-
+import function renderGallery(images) {
+  const markup = images.map((image) => {
+    // отримали значення і робимо деструктурізацію по ключовим значенням
+    const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = image
+    return `<li>
+	          <p><b>likes</b>: ${image.likes}</p>
+	          <p><b>views</b>: ${image.views}</p>
+	          <p><b>comments</b>: ${image.comments}</p>
+            <p><b>downloads</b>: ${image.downloads}</p>
+	        </li>`
+  }).join("separator");
 }
 // function renderUsers(users) {
 //   const markup = users
